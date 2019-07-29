0197aacce183f7a8b1686a6c94be0055356bb19d
FanrenCLI <fanrencli@163.com>
2019-07-29 09:37:59 +0800

    课表查询功能完结

--- pages/curriculum/curriculum.js
+++ pages/curriculum/curriculum.js
@@ -6,11 +6,16 @@ Page({
     { name: '4', class2: '' }, { name: '5', class2: '' }, { name: '6', class2: '' },
     { name: '7', class2: '' }, { name: '8', class2: '' }, { name: '9', class2: '' },
     { name: '10', class2: '' }, { name: '11', class2: '' }, { name: '12', class2: '' }],
-    week_kecheng: [{ week_day: [{ week: '一' }, { week: '二' }, { week: '三' }, { week: '四' }, { week: '五' }] }],
+    week_kecheng: [{ week_day: [{ week: '一' }, { week: '二' }, { week: '三' }, { week: '四' }, { week: '五' }, { week: '六' }, { week: '日' }] }],
     cardRightIn: false,
     cardLeftIn: false,
     currentIndex: 0,
     sw_kc: [],
+    multiArray: [[], []],
+    multiIndex: [0, 0],
+    index_multiArray: [[], []],
+    req_flag: false,
+    req_dep: false
   },
   onLoad: function (params) {
     var that = this
@@ -19,49 +24,23 @@ Page({
       icon: 'loading',
     });
     wx.request({
-      url: app.globalData.mainurl + 'curr',
+      url: app.globalData.mainurl + 'depart',
       data: {
-        stuid:app.globalData.stuid
-        // stuid: "1622022035"
+        cookie: app.globalData.localCookie
       },
       success(res) {
-        wx.hideToast();
-        that.data.sw_kc=[]
-        var currinfo = res.data
-        var bgindex = 0
-        for (var i in currinfo) {
-          var temp = {}
-          temp["kcmc"] = currinfo[i][0]
-          temp["skdd"] = currinfo[i][4]
-          temp["zhouci"] = currinfo[i][1]
-          switch (currinfo[i][2]) {
-            case '周一':
-              temp["xqj"] = 1
-              break;
-            case '周二':
-              temp["xqj"] = 2
-              break;
-            case '周三':
-              temp["xqj"] = 3
-              break;
-            case '周四':
-              temp["xqj"] = 4
-              break;
-            case '周五':
-              temp["xqj"] = 5
-              break;
+        if (res.data.length != 0) {
+          wx.hideToast();
+          that.data.multiArray[0] = []
+          that.data.index_multiArray[0] = []
+          for (var i in res.data) {
+            that.data.multiArray[0].push(res.data[i].text)
+            that.data.index_multiArray[0].push(res.data[i].val)
           }
-          var arrstr = currinfo[i][3].split(',')
-          temp["skjc"] = parseInt(arrstr[0]) 
-          temp['skcd'] = arrstr.length
-          temp["teacher"] = currinfo[i][6]
-          temp['bg'] = "color" + (bgindex % 4)
-          bgindex = bgindex + 1
-          that.data.sw_kc.push(temp)
+          that.setData({
+            multiArray: that.data.multiArray
+          })
         }
-        that.setData({
-          sw_kc:that.data.sw_kc
-        })
       },
       fail(res) {
         wx.showToast({
@@ -71,5 +50,109 @@ Page({
         })
       }
     })
+  },
+  bindMultiPickerChange: function (e) {
+    this.setData({
+      multiIndex: e.detail.value
+    })
+  },
+  bindMultiPickerColumnChange: function (e) {
+    var that = this
+    var data = {
+      multiArray: that.data.multiArray,
+      multiIndex: that.data.multiIndex
+    };
+    data.multiIndex[e.detail.column] = e.detail.value;
+    if (e.detail.column == 0 && that.data.req_dep == false) {
+      wx.request({
+        url: app.globalData.mainurl + 'major',
+        data: {
+          depid: that.data.index_multiArray[0][e.detail.value],
+          cookie: app.globalData.localCookie
+        },
+        method: "POST",
+        header: {
+          'content-type': "application/x-www-form-urlencoded"
+        },
+        success(res) {
+          that.data.multiArray[1] = []
+          that.data.index_multiArray[1] = []
+          if (res.data.length != 0) {
+            for (var i in res.data) {
+              that.data.multiArray[1].push(res.data[i].text)
+              that.data.index_multiArray[1].push(res.data[i].val)
+            }
+            that.setData({
+              multiArray: that.data.multiArray
+            })
+          }
+        },
+        fail(res) {
+          console.log(res.data)
+        }
+      })
+    }
+    that.setData(data);
+  },
+  CourseSearch: function (e) {
+    var that = this
+    var bjid = this.data.index_multiArray[1][this.data.multiIndex[1]]
+    var mydate = new Date()
+    var month = mydate.getMonth()
+    var year = mydate.getFullYear()
+    var term = ''
+    if (month > 8) {
+      term = year + '-' + (year + 1) + '-1'
+    } else {
+      term = (year - 1) + '-' + year + '-2'
+    }
+      wx.request({
+        url: app.globalData.mainurl + 'course',
+        data: {
+          cookie: app.globalData.localCookie,
+          bjid: bjid,
+          term: term
+        },
+        method: 'POST',
+        header: {
+          "content-type": "application/x-www-form-urlencoded"
+        },
+        success(res) {
+          console.log(res.data)
+          that.data.sw_kc = []
+          var result = res.data
+          for (var i in result) {
+            var myset = new Set()
+            for (var j in result[i]) {
+              if (result[i][j] != '') {
+                myset.add(result[i][j])
+                myset.add(j)
+              }
+            }
+            for (var n of myset) {
+              if (n.length > 2) {
+                var arr = n.split(' ')
+                var crr = { 'kcmc': arr[0], 'teacher': arr[1].substring(1), 'zhouci': arr[2].substring(1, arr[2].length - 1), 'skdd': arr[3].substring(0, arr[3].length - 1),'skjc':0,'xqj':parseInt(i)+1,'bg':'color'+parseInt(i+1)%4}
+                that.data.sw_kc.push(crr)
+              }else{
+                if(that.data.sw_kc[that.data.sw_kc.length-1].skjc==0){
+                  that.data.sw_kc[that.data.sw_kc.length-1].skjc=parseInt(n)
+                }else{
+                  that.data.sw_kc[that.data.sw_kc.length-1].skcd=parseInt(n)-that.data.sw_kc[that.data.sw_kc.length-1].skjc+1
+                }
+              }
+            }
+          }
+          that.data.req_flag = true
+          that.setData({
+            sw_kc:that.data.sw_kc
+          })
+        },
+        fail(res) {
+          console.log(res.data)
+        }
+      })
+    
+
   }
 })
--- pages/grade/grade.js
+++ pages/grade/grade.js
@@ -1,52 +1,56 @@
-let app =  getApp();
+let app = getApp();
 
-  Page({
-    data:{
-      termlist:['大一上学期','大一下学期','大二上学期','大二下学期','大三上学期','大三下学期','大四上学期','大四下学期'],
-      gradelist:[],
-      gradeToollist:[]
-    },
-    onLoad:function(options){
-        var that = this;
-        wx.setNavigationBarTitle({ title: '成绩查询' });
-        wx.showToast({
-          title: '查询中...',
-          icon: 'loading'
-        });
-        wx.request({
-          url:app.globalData.mainurl+'grade',
-          data:{
-            // cookie:'ASP.NET_SessionId=btb5g2rtdmddl2berxzlvcyb'
-            cookie:app.globalData.localCookie
-          },
-          method:'POST',
-          header: {
-            "Content-Type": "application/x-www-form-urlencoded"
-          },
-          success(res){
-            if(res.data!='0'){
-              that.setData({
-                gradelist:res.data
-              })
-              let termSet=new Set()
-              for(let i=0;i<res.data.length;i++){
-                termSet.add(res.data[i].xq)
-              }
-              let list= Array.from(termSet).sort()
-              that.setData({
-                gradeToollist:list,
-                termlist:that.data.termlist.slice(0,list.length)
-              })
-            }
-          },
-          fail(res){
-            wx.showToast({
-              title:'查询失败,请刷新',
-              icon:'none',
-              duration:3000
+Page({
+  data: {
+    termlist: ['大一上学期', '大一下学期', '大二上学期', '大二下学期', '大三上学期', '大三下学期', '大四上学期', '大四下学期'],
+    gradelist: [],
+    gradeToollist: [],
+    flag: false
+  },
+  onLoad: function (options) {
+    if (!this.data.flag) {
+      var that = this;
+      wx.setNavigationBarTitle({ title: '成绩查询' });
+      wx.showToast({
+        title: '查询中...',
+        icon: 'loading'
+      });
+      wx.request({
+        url: app.globalData.mainurl + 'grade',
+        data: {
+          // cookie:'ASP.NET_SessionId=btb5g2rtdmddl2berxzlvcyb'
+          cookie: app.globalData.localCookie
+        },
+        method: 'POST',
+        header: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        success(res) {
+          if (res.data != '0') {
+            that.setData({
+              gradelist: res.data
             })
-            console.log("查询成绩失败")
+            let termSet = new Set()
+            for (let i = 0; i < res.data.length; i++) {
+              termSet.add(res.data[i].xq)
+            }
+            let list = Array.from(termSet).sort()
+            that.setData({
+              gradeToollist: list,
+              termlist: that.data.termlist.slice(0, list.length),
+              flag:true
+            })
           }
-        })
+        },
+        fail(res) {
+          wx.showToast({
+            title: '查询失败,请刷新',
+            icon: 'none',
+            duration: 3000
+          })
+          console.log("查询成绩失败")
+        }
+      })
     }
-  })
+  }
+})
